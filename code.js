bc7d93e6794f8f007449bb31e4c39cf5d65ce3a9
Developer_2700 <66629947+UtkirKurbanov@users.noreply.github.com>
2022-10-11 11:37:46 +0500

    Update code.js

--- code.js
+++ code.js
@@ -5,3 +5,21 @@ document.querySelector("#alert").addEventListener("click", function () {
 document.querySelector("#confirm").addEventListener("click", function () {
   confirm("Are you sure you want to press the button!");
 });
+
+document.querySelector("#prompt").addEventListener("click", function () {
+  var age = prompt("Please add your age", 18);
+
+  if (age >= 18) {
+    alert("You can get through");
+  } else {
+    alert("You are young");
+  }
+});
+
+// throw new Error("Error"); //console working untill 23row
+console.error("Console.error");
+
+console.log("Console.log");
+console.warn("Console.warn");
+console.info("Console.info");
+
