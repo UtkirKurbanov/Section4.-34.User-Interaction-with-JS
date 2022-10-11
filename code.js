3ac44cb405389e4d086d123f8ce293223e546b6d
Utkir <utkirkurbaov2700@gmail.com>
2022-10-11 10:54:56 +0500

    1-st commit

---
+++ code.js
@@ -0,0 +1,7 @@
+document.querySelector("#alert").addEventListener("click", function () {
+  alert("You have successfully clicked the button!");
+});
+
+document.querySelector("#confirm").addEventListener("click", function () {
+  confirm("Are you sure you want to press the button!");
+});
